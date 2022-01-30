4d163c2c3134dc82aaf44f6bcca7192b261c49f4
Daniele Nicolodi <daniele@grinta.net>
2022-01-30 23:56:36 +0100

    Make tests self contained
    Embed the test ledger into the test harnesses. Make the test ledger
    much smaller and thus the tests easier to verify. This also allows to
    get rid of the example.beancount file and of the code required to find
    it and makes the tests work when the package is installed.

--- beanquery/test_utils.py
+++
@@ -1,7 +0,0 @@
-__copyright__ = "Copyright (C) 2021  Martin Blais"
-__license__ = "GNU GPLv2"
-
-from os import path
-
-EXAMPLE_LEDGER_PATH = path.normpath(
-    path.join(path.dirname(__file__), '..', 'examples', 'example.beancount'))
