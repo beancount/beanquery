6799c9866561fcfb4632c19eeb0b578b0374fc06
Daniele Nicolodi <daniele@grinta.net>
2022-04-12 01:11:06 +0200

    tables: First stab at implementing a Table object for BQL

---
+++ beanquery/tables.py
@@ -0,0 +1,48 @@
+from beanquery import query_env
+
+
+class Table:
+    columns = {}
+    functions = {}
+
+    def __iter__(self):
+        raise NotImplementedError
+
+    def get_column(self, name):
+        """Return a column accessor for the given named column.
+        Args:
+          name: A string, the name of the column to access.
+        """
+        col = self.columns[name]
+        if col is not None:
+            return col()
+
+        raise CompilationError(f'Invalid column name "{name}" in table "{self.name}')
+
+    def get_function(self, name, operands):
+        """Return a function accessor for the given named function.
+        Args:
+          name: A string, the name of the function to access.
+        """
+        func = types.function_lookup(self.functions, name, operands)
+        if func is not None:
+            return func(operands)
+
+        sig = '{}({})'.format(name, ', '.join(operand.dtype.__name__ for operand in operands))
+        raise CompilationError(f'Unknown function "{sig}" in {self.context_name}')
+
+
+class NullTable(Table):
+    functions = query_env.SIMPLE_FUNCTIONS
+
+    def __iter__(self):
+        yield from iter([None])
+
+
+TABLES = {
+     '_': NullTable(),
+}
+
+
+def get(name):
+    return TABLES.get(name)
