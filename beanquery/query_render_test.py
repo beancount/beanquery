f3676e9bbc650fa3d7aa3b71f1a3c1c673eb8058
Daniele Nicolodi <daniele@grinta.net>
2023-01-16 23:31:43 +0100

    render: Fix rendering of columns containing only NULL values
    This requires reworking how the width of the columns and postponing
    padding to rendered row assembly time.
    
    Fixes #125.

--- beanquery/query_render_test.py
+++ beanquery/query_render_test.py
@@ -9,50 +9,20 @@ import unittest
 from decimal import Decimal
 
 from beancount.core import display_context
-from beancount.core.amount import A
+from beancount.core.amount import Amount, A
 from beancount.core.inventory import Inventory, from_string as I
 from beancount.core.number import D
-from beancount.core.position import from_string as P
+from beancount.core.position import Position, from_string as P
 
 from beanquery import query_render
 
-def flatten(values):
-    # Flatten lists of lists.
-    for x in values:
-        if isinstance(x, list):
-            yield from flatten(x)
-        else: yield x
 
+class TestColumnRenderer(unittest.TestCase):
 
-class ColumnRendererBase(unittest.TestCase):
-
-    # pylint: disable=not-callable
-    renderer = None
-
-    def setUp(self):
+    def test_column_renderer(self):
         dcontext = display_context.DisplayContext()
-        self.ctx = query_render.RenderContext(dcontext, expand=True)
-
-    def prepare(self, values):
-        renderer = self.renderer(self.ctx)
-        for value in values:
-            renderer.update(value)
-        width = renderer.prepare()
-        return renderer, width
-
-    def render(self, values):
-        renderer, width = self.prepare(values)
-        strings = [renderer.format(value) for value in values]
-        self.assertTrue(all(len(s) == width for s in flatten(strings)))
-        return strings
-
-
-class TestColumnRenderer(ColumnRendererBase):
-
-    renderer = query_render.ColumnRenderer
-
-    def test_interface(self):
-        renderer = self.renderer(self.ctx)
+        ctx = query_render.RenderContext(dcontext)
+        renderer = query_render.ColumnRenderer(ctx)
         with self.assertRaises(RuntimeError):
             w = renderer.width
         w = renderer.prepare()
@@ -62,141 +32,132 @@ class TestColumnRenderer(ColumnRendererBase):
             renderer.format(None)
 
 
-class ObjectRenderer(ColumnRendererBase):
+class RendererTestBase(unittest.TestCase):
 
-    renderer = query_render.ObjectRenderer
+    def setUp(self):
+        self.dcontext = display_context.DisplayContext()
+
+    def render(self, dtype, values, **kwargs):
+        out = io.StringIO()
+        rows = [(x, ) for x in values]
+        query_render.render_text([('', dtype)], rows, self.dcontext, out, **kwargs)
+        return out.getvalue().splitlines()[2:]
+
+
+class TestRenderer(RendererTestBase):
 
     def test_object(self):
-        self.assertEqual(self.render(["foo", 1, D('1.23'), datetime.date(1970, 1, 1)]), [
+        self.assertEqual(self.render(object, ["foo", 1, D('1.23'), datetime.date(1970, 1, 1)]), [
             'foo       ',
             '1         ',
             '1.23      ',
             '1970-01-01',
         ])
 
-
-class TestBoolRenderer(ColumnRendererBase):
-
-    renderer = query_render.BoolRenderer
-
     def test_bool(self):
-        self.assertEqual(self.render([True, True]), [
+        self.assertEqual(self.render(bool, [True, True]), [
             'TRUE',
             'TRUE',
         ])
-        self.assertEqual(self.render([False, True]), [
+        self.assertEqual(self.render(bool, [False, True]), [
             'FALSE',
             'TRUE ',
         ])
-        self.assertEqual(self.render([False, False]), [
+        self.assertEqual(self.render(bool, [False, False]), [
             'FALSE',
             'FALSE',
         ])
 
-
-class TestStringRenderer(ColumnRendererBase):
-
-    renderer = query_render.StringRenderer
-
-    def test_string(self):
-        self.assertEqual(self.render(['a', 'bb', 'ccc', '']), [
+    def test_str(self):
+        self.assertEqual(self.render(str, ['a', 'bb', 'ccc', '']), [
             'a  ',
             'bb ',
             'ccc',
             '   ',
         ])
 
-
-class TestSetRenderer(ColumnRendererBase):
-
-    renderer = query_render.SetRenderer
-
-    def test_stringset(self):
-        self.ctx.listsep = '+'
-        self.assertEqual(self.render([{}, {'aaaa'}, {'bb', 'ccc'}]), [
-            '      ',
-            'aaaa  ',
-            'bb+ccc',
+    def test_str_null(self):
+        self.assertEqual(self.render(str, ['', None]), [
+            ' ',
+            ' ',
+        ])
+        self.assertEqual(self.render(str, ['', None], null='NULL'), [
+            '    ',
+            'NULL',
         ])
 
-
-class TestDateRenderer(ColumnRendererBase):
-
-    renderer = query_render.DateRenderer
+    def test_set_str(self):
+        self.assertEqual(self.render(set, [{}, {'aaaa'}, {'bb', 'ccc'}]), [
+            '       ',
+            'aaaa   ',
+            'bb  ccc',
+        ])
 
     def test_date(self):
-        self.assertEqual(self.render([datetime.date(2014, 10, 3)]), ['2014-10-03'])
+        self.assertEqual(self.render(datetime.date, [datetime.date(2014, 10, 3)]), [
+            '2014-10-03'
+        ])
 
-
-class TestIntRenderer(ColumnRendererBase):
-
-    renderer = query_render.IntRenderer
-
-    def test_integer(self):
-        self.assertEqual(self.render([1, 22, 333]), [
+    def test_int(self):
+        self.assertEqual(self.render(int, [1, 22, 333]), [
             '  1',
             ' 22',
             '333',
         ])
-        self.assertEqual(self.render([1, -22, 333]), [
+        self.assertEqual(self.render(int, [1, -22, 333]), [
             '  1',
             '-22',
             '333',
         ])
-        self.assertEqual(self.render([1, 22, -333]), [
+        self.assertEqual(self.render(int, [1, 22, -333]), [
             '   1',
             '  22',
             '-333',
         ])
 
-
-class TestDecimalRenderer(ColumnRendererBase):
-
-    renderer = query_render.DecimalRenderer
-
-    def test_integral(self):
-        self.assertEqual(self.render([D('1'), D('12'), D('123'), D('1e4')]), [
+    def test_decimal_integral(self):
+        self.assertEqual(self.render(Decimal, [D('1'), D('12'), D('123'), D('1e4')]), [
             '   1',
             '  12',
             ' 123',
             '1E+4',
         ])
-        self.assertEqual(self.render([D('1'), D('-12'), D('123')]), [
+        self.assertEqual(self.render(Decimal, [D('1'), D('-12'), D('123')]), [
             '  1',
             '-12',
             '123',
         ])
-        self.assertEqual(self.render([D('1'), D('12'), D('-123')]), [
+        self.assertEqual(self.render(Decimal, [D('1'), D('12'), D('-123')]), [
             '   1',
             '  12',
             '-123',
         ])
-        self.assertEqual(self.render([D('1'), D('12'), D('-1e3')]), [
+        self.assertEqual(self.render(Decimal, [D('1'), D('12'), D('-1e3')]), [
             '    1',
             '   12',
             '-1E+3',
         ])
 
-    def test_fractional(self):
-        self.assertEqual(self.render([D('0.1'), D('1.2'), D('1.23'), D('1.234')]), [
+    def test_decimal_fractional(self):
+        self.assertEqual(self.render(Decimal, [D('0.1'), D('1.2'), D('1.23'), D('1.234')]), [
             '0.1  ',
             '1.2  ',
             '1.23 ',
             '1.234',
         ])
-        self.assertEqual(self.render([D('12'), D('1.2'), D('1.23'), D('12.345')]), [
+        self.assertEqual(self.render(Decimal, [D('12'), D('1.2'), D('1.23'), D('12.345')]), [
             '12    ',
             ' 1.2  ',
             ' 1.23 ',
             '12.345',
         ])
-        self.assertEqual(self.render([D('12'), D('1.2'), D('1.23'), D('-12.345')]), [
+        self.assertEqual(self.render(Decimal, [D('12'), D('1.2'), D('1.23'), D('-12.345')]), [
             ' 12    ',
             '  1.2  ',
             '  1.23 ',
             '-12.345',
         ])
-        self.assertEqual(self.render([D('12'), D('1.2'), D('1.23'), D('-1.2e3')]), [
+        self.assertEqual(self.render(Decimal, [D('12'), D('1.2'), D('1.23'), D('-1.2e3')]), [
             '     12   ',
             '      1.2 ',
             '      1.23',
@@ -204,23 +165,25 @@ class TestDecimalRenderer(ColumnRendererBase):
         ])
 
 
-class TestAmountRenderer(ColumnRendererBase):
+class TestAmountRenderer(RendererTestBase):
 
-    renderer = query_render.AmountRenderer
+    def render(self, values, **kwargs):  # pylint: disable=arguments-differ
+        return super().render(Amount, values, **kwargs)
 
     def test_amount(self):
-        self.assertEqual(self.render([A('100.00 USD')]), ['100.00 USD'])
+        self.assertEqual(
+            self.render([A('100.00 USD')]), ['100.00 USD'])
 
     def test_quantization_one(self):
-        self.ctx.dcontext.update(Decimal('1.0000'), 'ETH')
-        self.assertEqual(self.ctx.dcontext.quantize(Decimal('1.0'), 'ETH'), Decimal('1.0000'))
+        self.dcontext.update(Decimal('1.0000'), 'ETH')
+        self.assertEqual(self.dcontext.quantize(Decimal('1.0'), 'ETH'), Decimal('1.0000'))
         self.assertEqual(self.render([A('1 ETH')]), ['1.0000 ETH'])
         self.assertEqual(self.render([A('0.00001 ETH')]), ['0.0000 ETH'])
 
     def test_quantization_many(self):
-        self.ctx.dcontext.update(Decimal('1.0000'), 'ETH')
-        self.ctx.dcontext.update(Decimal('1.00'), 'USD')
-        self.ctx.dcontext.update(Decimal('1'), 'XYZ')
+        self.dcontext.update(Decimal('1.0000'), 'ETH')
+        self.dcontext.update(Decimal('1.00'), 'USD')
+        self.dcontext.update(Decimal('1'), 'XYZ')
         self.assertEqual(self.render([A('1.0 ETH')]), ['1.0000 ETH'])
         self.assertEqual(self.render([A('1.0 USD')]), ['1.00 USD'])
         self.assertEqual(self.render([A('1.0 XYZ')]), ['1 XYZ'])
@@ -265,19 +228,20 @@ class TestAmountRenderer(ColumnRendererBase):
         ])
 
 
-class TestPositionRenderer(ColumnRendererBase):
+class TestPositionRenderer(RendererTestBase):
 
-    renderer = query_render.PositionRenderer
+    def render(self, values, **kwargs):  # pylint: disable=arguments-differ
+        return super().render(Position, values, **kwargs)
 
     def setUp(self):
         super().setUp()
         # Prime the display context for some known commodities.
-        self.ctx.dcontext = display_context.DisplayContext()
-        self.ctx.dcontext.update(D('1.00'), 'USD')
-        self.ctx.dcontext.update(D('1.00'), 'CAD')
-        self.ctx.dcontext.update(D('1.000'), 'HOOL')
-        self.ctx.dcontext.update(D('1'), 'CA')
-        self.ctx.dcontext.update(D('1.00'), 'AAPL')
+        self.dcontext = display_context.DisplayContext()
+        self.dcontext.update(D('1.00'), 'USD')
+        self.dcontext.update(D('1.00'), 'CAD')
+        self.dcontext.update(D('1.000'), 'HOOL')
+        self.dcontext.update(D('1'), 'CA')
+        self.dcontext.update(D('1.00'), 'AAPL')
 
     def test_simple_poitions(self):
         self.assertEqual(self.render([P('3.0 USD'), P('3.0 CAD'), P('3.0 HOOL'), P('3.0 CA'), P('3.0 AAPL'), P('3.0 XY')]), [
@@ -296,19 +260,20 @@ class TestPositionRenderer(ColumnRendererBase):
         ])
 
 
-class TestInventoryRenderer(ColumnRendererBase):
+class TestInventoryRenderer(RendererTestBase):
 
-    renderer = query_render.InventoryRenderer
+    def render(self, values, **kwargs):  # pylint: disable=arguments-differ
+        return super().render(Inventory, values, **kwargs)
 
     def setUp(self):
         super().setUp()
         # Prime the display context for some known commodities.
-        self.ctx.dcontext = display_context.DisplayContext()
-        self.ctx.dcontext.update(D('1.00'), 'USD')
-        self.ctx.dcontext.update(D('1.00'), 'CAD')
-        self.ctx.dcontext.update(D('1.000'), 'HOOL')
-        self.ctx.dcontext.update(D('1'), 'CA')
-        self.ctx.dcontext.update(D('1.00'), 'AAPL')
+        self.dcontext = display_context.DisplayContext()
+        self.dcontext.update(D('1.00'), 'USD')
+        self.dcontext.update(D('1.00'), 'CAD')
+        self.dcontext.update(D('1.000'), 'HOOL')
+        self.dcontext.update(D('1'), 'CA')
+        self.dcontext.update(D('1.00'), 'AAPL')
 
     def test_position_sortkey(self):
         inventory = I('1 AAAAA, 5 SHARE {100 USD}, 5 SHARE {200 USD}, 5 TESTS {666 USD}')
@@ -321,46 +286,41 @@ class TestInventoryRenderer(ColumnRendererBase):
             ])
 
     def test_inventory(self):
-        self.ctx.expand = True
-        self.assertEqual(self.render([I('100 USD')]),[
-            ['100.00 USD']
+        self.assertEqual(self.render([I('100 USD')], expand=True),[
+            '100.00 USD'
         ])
-        self.assertEqual(self.render([I('5 HOOL {500.23 USD}')]), [
-            ['5.000 HOOL {500.23 USD}']
+        self.assertEqual(self.render([I('5 HOOL {500.23 USD}')], expand=True), [
+            '5.000 HOOL {500.23 USD}'
         ])
-        self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 12.3456 CAAD')]), [
-            ['12.3456 CAAD             ',
-             ' 5.000  HOOL {500.23 USD}'],
+        self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 12.3456 CAAD')], expand=True), [
+            '12.3456 CAAD             ',
+            ' 5.000  HOOL {500.23 USD}',
         ])
 
     def test_inventory_tabular(self):
-        self.ctx.expand = False
-        self.ctx.listsep = ' & '
-        self.assertEqual(self.render([I('100 USD')]), [
+        self.assertEqual(self.render([I('100 USD')], expand=False, listsep=' & '), [
             '100.00 USD'
         ])
-        self.assertEqual(self.render([I('5 HOOL {500.23 USD}')]), [
+        self.assertEqual(self.render([I('5 HOOL {500.23 USD}')], expand=False, listsep=' & '), [
             '5.000 HOOL {500.23 USD}'
         ])
-        self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 12.3456 CAAD')]), [
+        self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 12.3456 CAAD')], expand=False, listsep=' & '), [
             '12.3456 CAAD & 5.000 HOOL {500.23 USD}',
         ])
         self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 12.3456 CAAD'),
-                                      I('55 HOOL {50.23 USD}, 2.3 CAAD')]), [
+                                      I('55 HOOL {50.23 USD}, 2.3 CAAD')], expand=False, listsep=' & '), [
             '12.3456 CAAD &  5.000 HOOL {500.23 USD}',
             ' 2.3000 CAAD & 55.000 HOOL { 50.23 USD}',
         ])
         self.assertEqual(self.render([I('5 HOOL {500.23 USD}, 1 HOOL {567.89 USD}'),
-                                      I('55 HOOL {50.23 USD}, 2.3 CAAD')]), [
+                                      I('55 HOOL {50.23 USD}, 2.3 CAAD')], expand=False, listsep=' & '), [
             '         &  5.000 HOOL {500.23 USD} &  1.000 HOOL {567.89 USD}',
             '2.3 CAAD & 55.000 HOOL { 50.23 USD} &                         ',
         ])
 
     def test_inventory_too_many(self):
-        self.ctx.expand = False
-        self.ctx.listsep = ' & '
         self.assertEqual(self.render([I('10 AA, 2 BB, 3 CC, 4 DD'),
-                                      I('5 AA, 6 EE, 7 FF')]), [
+                                      I('5 AA, 6 EE, 7 FF')], expand=False, listsep=' & '), [
             '10 AA & 2 BB & 3 CC & 4 DD              ',
             ' 5 AA & 6 EE & 7 FF                     ',
         ])
@@ -508,8 +468,8 @@ class TestQueryRenderCSV(unittest.TestCase):
             [(None, 2, 3), (4, None, 6)]), textwrap.dedent(
                 """\
                 x,y,z
-                 ,2,3
-                4, ,6
+                ,2,3
+                4,,6
                 """))
 
     def test_render_expand(self):
@@ -519,9 +479,9 @@ class TestQueryRenderCSV(unittest.TestCase):
              (33, I('2.00 EUR, 42 TESTS'), 4)], expand=True), "\n".join([
                  "x,inv,q",
                  "11, 1.00 EUR  ,2",
-                 "  , 1    TESTS, ",
-                 "  , 1.00 USD  , ",
+                 ", 1    TESTS,",
+                 ", 1.00 USD  ,",
                  "33, 2.00 EUR  ,4",
-                 "  ,42    TESTS, ",
+                 ",42    TESTS,",
                  "",
              ]))
