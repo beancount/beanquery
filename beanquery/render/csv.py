d08702b2dab2a8894146e3d864ba1e6f37baf73a
Daniele Nicolodi <daniele@grinta.net>
2024-02-23 22:19:08 +0100

    render: Fix CSV rendering
    Add minimal test to catch this sort of bugs in the future.
    
    Fixes #167.

--- beanquery/render/csv.py
+++ beanquery/render/csv.py
@@ -2,4 +2,4 @@ from ..query_render import render_csv
 
 
 def render(desc, rows, file, *, dcontext, **kwargs):
-    return render_csv(desc, rows, file, dcontext, **kwargs)
+    return render_csv(desc, rows, dcontext, file, **kwargs)
