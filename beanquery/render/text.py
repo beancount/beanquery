0d6b1b65ae016aef7f9adbde9bda54877f291097
Daniele Nicolodi <daniele@grinta.net>
2025-01-30 11:39:24 +0100

    render.text: Do not emit "(empty)" for empty result sets
    It is not particularly useful, and AFAIK it is not implemented by any
    other data query shell. This allows to handle statements expected to
    return empty result set the same way as SELECTs. Emitting "(empty)"
    when executing CREATE TABLE or INSERT statements is weird.

--- beanquery/render/text.py
+++ beanquery/render/text.py
@@ -3,5 +3,5 @@ from ..query_render import render_text
 
 def render(desc, rows, file, *, dcontext, **kwargs):
     if not rows:
-        return print("(empty)", file=file)
+        return
     return render_text(desc, rows, dcontext, file, **kwargs)
